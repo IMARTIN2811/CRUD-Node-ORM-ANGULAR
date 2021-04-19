e932ddd40c003da94db303c95abd532a5c82c18c
IMARTIN <israel_2896@hotmail.com>
2021-04-19 10:15:12 -0500

    Modificacion de headers

--- my-app-product/src/app/Components/header/header.component.ts
+++ my-app-product/src/app/Components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+//EventEmitter permite crear eventos personalizados
+//
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  //variables para mostrar y ocultar sidenav
+  varAdmin = false;
+
+  //crea la propiedad togglerSidenav y despues crea la instancia de eventemitter
+  @Output() togglerSidenav = new EventEmitter<void>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  //crea el metodo 
+  onToggleSidenav(): void{
+    //cuando el user de clic en el header va mostrar el toggler
+    this.togglerSidenav.emit();
+  }
 }
