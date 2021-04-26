d5c130afe75673112e425258802bd1042516b4f9
IMARTIN <israel_2896@hotmail.com>
2021-04-26 16:39:17 -0500

    Se importaron las nuevas rutas en server.js y creacion de roles

--- node-server/server.js
+++ node-server/server.js
@@ -16,24 +16,43 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//se agregan las rutas en el server.js
+require("./app/routes/product.routes")(app);
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
+
 const db = require("./app/models");
+const Role = db.role;
 
 db.sequelize.sync();
 // // drop the table if it already exists
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
+//db.sequelize.sync({ force: true }).then(() => {
+//  console.log("Drop and re-sync db.");
+//  initial(); 
+//});
 
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Bienvenido!!" });
 });
 
-//require("./app/routes/turorial.routes")(app);
-require("./app/routes/product.routes")(app);
-
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+function initial(){
+  Role.create({
+    id: 1,
+    name: "user"
+  }),
+  Role.create({
+    id: 2,
+    name: "employee"
+  });
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
