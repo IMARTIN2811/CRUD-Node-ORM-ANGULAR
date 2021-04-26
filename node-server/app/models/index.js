19429530a842cbc9f3206f9cbaf70a126003396b
IMARTIN <israel_2896@hotmail.com>
2021-04-26 16:36:57 -0500

    Se definieron los modelos user y role

--- node-server/app/models/index.js
+++ node-server/app/models/index.js
@@ -21,5 +21,24 @@ db.sequelize = sequelize;
 
 db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
 db.products = require("./product.model.js")(sequelize,Sequelize);
+db.users = require("./User.model.js")(sequelize,Sequelize);
+db.role = require("./role.model.js")(sequelize,Sequelize);
+
+//indica que el modelo de usuario puede pertenecer a muchos roles y viceversa.
+db.role.belongsToMany(db.users, {
+  //Con through, foreignKey, otherKey, vamos a tener una nueva tabla user_roles como conexión entre los usuarios y la tabla de roles a través de su clave principal como claves foráneas.
+  through: "user_roles",
+  foreignKey: "roleId",
+  otherKey: "userId",
+});
+
+//Usamos User.belongsToMany(Role)para indicar que el modelo de usuario puede pertenecer a muchos roles y viceversa.
+db.users.belongsToMany(db.role,{
+  through: "user_roles",
+  foreignKey: "userId",
+  otherKey: "roleId"
+});
+
+db.ROLES = ["user","admin", "employee"];
 
 module.exports = db;
