ea7cbfca332c9caf0d5ecc9320a109d13cb7db77
IMARTIN <israel_2896@hotmail.com>
2021-07-07 17:32:51 -0500

    Actualización del componente inicio

--- my-app-product/src/app/Components/Menu/inicio/inicio.component.ts
+++ my-app-product/src/app/Components/Menu/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-inicio',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+  public data = [];
 
-  constructor() { }
+  constructor(private http: HttpClient ) { }
 
   ngOnInit(): void {
+    this.getImages();
+  }
+  
+  getImages(){
+    this.data = [];
+    this.http.get('http://localhost:8080/api/images/all')
+      .subscribe(res =>{
+        console.log(res);
+        for (const dd of res['data']){
+          var imgUrl = btoa(dd.image.data.reduce((data,byte)=> data + String.fromCharCode(byte), ''))
+          this.data.push({ id: dd.image_id, description: dd.description, url: imgUrl });
+        }
+      })
   }
 
 }
